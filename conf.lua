6112253f4638d5f8db88fcd45220aa723891ef7b
PixelwarStudio <janko.matthes@gmx.net>
2016-09-08 22:52:04 +0200

    set version and some window properties

--- conf.lua
+++ conf.lua
@@ -1,6 +1,10 @@
 function love.conf(t)
-    t.console = false
+    t.version = '0.10.1'
+    
     t.window.title = 'Simple Fractal Tree Generator'
     t.window.width = 1000
     t.window.height = 600
+    t.window.minwidth = 800
+    t.window.minheight = 480
+    t.window.resizable = true
 end
