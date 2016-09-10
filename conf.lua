e73a1115e2841b2a8e75a5664bb06dc7665bbe3c
PixelwarStudio <janko.matthes@gmx.net>
2016-09-10 11:13:45 +0200

    adjust window borders

--- conf.lua
+++ conf.lua
@@ -3,8 +3,8 @@ function love.conf(t)
     
     t.window.title = 'Simple Fractal Tree Generator'
     t.window.width = 1000
-    t.window.height = 560
+    t.window.height = 600
     t.window.minwidth = 800
-    t.window.minheight = 560
+    t.window.minheight = 600
     t.window.resizable = true
 end
