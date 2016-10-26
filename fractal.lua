1c2569dc9626e25777670818c02d5d8c3652a04b
PixelwarStudio <janko.matthes@gmx.net>
2016-10-26 19:54:39 +0200

    add branches parameter to fractal edit affected function to fit to the new paramater

--- fractal.lua
+++ fractal.lua
@@ -36,11 +36,12 @@ end
 
 -- Fractal
 local Fractal = Class('Fractal')
-function Fractal:initialize(x, y, length, scale, angle)
+function Fractal:initialize(x, y, length, scale, angle, branches)
     self.pos = Vector(x, y)
     self.len = length
     self.scale = scale
     self.angle = angle
+    self.branches = branches
 
     self.iter = 0
     self.nodes = {
@@ -48,12 +49,12 @@ function Fractal:initialize(x, y, length, scale, angle)
     }
 end
 
-local function calcNodes(iteration)
-    return math.pow(2, iteration)
+local function calcNodes(branches, iteration)
+    return math.pow(branches, iteration)
 end
 
 function Fractal:calcNodes()
-    return calcNodes(self.iter)
+    return calcNodes(self.branches, self.iter)
 end
 
 function Fractal:calcDim()
@@ -71,12 +72,14 @@ end
 
 function Fractal:iterate()
     local olderNodes = #self.nodes
-    for n = 0, calcNodes(self.iter) - 1 do
+    for n = 0, calcNodes(self.branches, self.iter) - 1 do
         local pos, dim = self.nodes[olderNodes - n]:unpack()
         local trimDim = dim:trimmed(dim:len() * self.scale)
+        local angleBranch = 2 * self.angle / (self.branches - 1)
 
-        table.insert(self.nodes, Node(pos + dim, trimDim:rotated(self.angle)))
-        table.insert(self.nodes, Node(pos + dim, trimDim:rotated(2 * math.pi - self.angle)))
+        for branch = 1, self.branches do
+            table.insert(self.nodes, Node(pos + dim, trimDim:rotated(self.angle - angleBranch * (branch-1))))
+        end
     end
     self.iter = self.iter + 1
 end
