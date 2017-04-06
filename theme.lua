5712dcd820a613af526aa65de42255b37a2d2114
Pixelwar <janko.matthes@gmx.net>
2017-04-06 23:14:29 +0200

    Transfer suit theme in extra file

---
+++ theme.lua
@@ -0,0 +1,10 @@
+local Suit = require('lib.suit')
+
+Suit.theme.cornerRadius = 0
+Suit.theme.color = {
+    normal  = {bg = {218, 40, 40}, fg = {0, 0, 0}},
+    hovered = {bg = {218, 22, 22}, fg = {0, 0, 0}},
+    active  = {bg = {218, 6, 6}, fg = {0, 0, 0}}
+}
+
+return Suit
